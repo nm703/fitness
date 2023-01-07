81f504d2f01393ef733aa34a2e29947e094b2fa4
Nevena Markovic <nevenamarkovic703@gmail.com>
2023-01-07 21:45:32 +0100

    done_detail_similar

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExerciseVideos() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExerciseVideos
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Stack } from '@mui/material';
+import { InfinitySpin } from 'react-loader-spinner';
 
 function Loader() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+            <InfinitySpin color="grey" />
+        </Stack>
+    )
 }
 
 export default Loader
