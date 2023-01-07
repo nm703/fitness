5e7aa1cafa9ffc636800ee8e200aaff4fa2d32e7
Nevena Markovic <nevenamarkovic703@gmail.com>
2023-01-07 21:26:36 +0100

    added_search_api_data_navbar_skeleton

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExerciseCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExerciseCard
---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HorizontalScrollbar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HorizontalScrollbar
